ee851980896bab7eeeae9eb73e168e551d19973b
Mani <Ramachandran>
2021-09-30 20:46:48 -0400

    added tests

---
+++ block.test.js
@@ -0,0 +1,24 @@
+const Block = require('./block');
+
+describe('Block', () => {
+  let data, lastBlock, block;
+
+  beforeEach(() => {
+    data = {
+      sender: 'Steven Cox ',
+      recepient: 'Mani Ramachandran',
+      quantity: 6,
+    };
+
+    lastBlock = Block.genesis();
+    block = Block.createBlock(lastBlock, data);
+  });
+
+  it('sets the `data` to match the input', () => {
+    expect(block.data).toStrictEqual(data);
+  });
+
+  it('sets the `lasthash` to match the hash of the last block', () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+});
---
+++ blockchain.test.js
@@ -0,0 +1,25 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+  let bc;
+
+  beforeEach(() => {
+    bc = new Blockchain();
+  });
+
+  it('starts with genesis block', () => {
+    expect(bc.chain[0]).toEqual(Block.genesis());
+  });
+
+  it('adds a new block', () => {
+    const data = {
+      sender: 'Steven Cox ',
+      recepient: 'Mani Ramachandran',
+      quantity: 6,
+    };
+    bc.addBlock(data);
+
+    expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+  });
+});
