447b12d5c2f4bb8efdede7ba2e2478ac262cc66e
Mani <Ramachandran>
2021-10-12 12:22:43 -0400

    get valid transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -2,6 +2,7 @@
   This class is responsible for collecting transactions from multiple users.
 */
 
+const Transaction = require('./transaction');
 class TransactionPool {
   constructor() {
     this.transactions = [];
@@ -19,6 +20,25 @@ class TransactionPool {
 
   existingTransaction(address) {
     return this.transactions.find((t) => t.input.address === address);
+  }
+
+  validTransactions() {
+    return this.transactions.filter((tx) => {
+      const outputTotal = tx.outputs.reduce((total, output) => {
+        return total + output.amount;
+      }, 0);
+
+      if (tx.input.amount !== outputTotal) {
+        console.log(`Invalid transaction from ${tx.input.address}.`);
+        return;
+      }
+
+      if (!Transaction.verifyTransaction(tx)) {
+        console.log(`Invalid signature from ${tx.input.address}.`);
+        return;
+      }
+      return tx;
+    });
   }
 }
 
